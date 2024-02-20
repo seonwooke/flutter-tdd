d01d51255fb86aeba421c27c2fa238f41c82cc4c
seonwooke <seonookkim@gmail.com>
2024-02-20 23:12:47 +0900

    :zap: Flutter Test Driven Development
    Flutter Test Driven Development

---
+++ lib/scafflod_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ScaffoldWidget extends StatelessWidget {
+  const ScaffoldWidget({
+    super.key,
+    required this.title,
+    required this.message,
+  });
+
+  final String title;
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Text(message),
+        ),
+      ),
+    );
+  }
+}
---
+++ test/unit_test.dart
@@ -0,0 +1,8 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('should be lowercase', () {
+    String hello = "Hello World";
+    expect(hello.toLowerCase(), "hello worlD");
+  });
+}
